bf0973cc1640a81fce951242c65f97c3d5b75b93
jiaqiguo <422532105@qq.com>
2018-05-15 19:37:54 -0300

    Assignment1

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.acme.csci3130_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_TEXT = "com.example.acme.csci3130_demo.TEXT";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendText(View view){
+        Intent intent = new Intent(this, DisplayTextActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText3);
+        String text = editText.getText().toString();
+        intent.putExtra(EXTRA_TEXT, text);
+        startActivity(intent);
     }
 }
