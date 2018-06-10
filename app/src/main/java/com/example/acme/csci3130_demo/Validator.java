1c6bab32bd65f7a9ae728d78427a85e26374152f
jiaqiguo <422532105@qq.com>
2018-06-09 22:12:20 -0300

    a2 stage2

--- app/src/main/java/com/example/acme/csci3130_demo/Validator.java
+++ app/src/main/java/com/example/acme/csci3130_demo/Validator.java
@@ -1,5 +1,8 @@
 package com.example.acme.csci3130_demo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validator {
 
     public static int validation(String pw) {
@@ -10,6 +13,27 @@ public class Validator {
         }
         //it is at least 8 characters long
         if (!(pw.length() < 8)) {
+            rules++;
+        }
+        //requiring at least 1 special character
+        String reg1 ="[*.!@#$%^&(){}\\[]\\:;<>,.?/~_+-=|\\]";
+        Pattern pattern1 = Pattern.compile(reg1);
+        Matcher matcher1 = pattern1.matcher(pw);
+        if(matcher1.find()==true){
+            rules++;
+        }
+        String reg2 ="[0-9]";
+        Pattern pattern2 = Pattern.compile(reg2);
+        Matcher matcher2 = pattern2.matcher(pw);
+        //at least 1 digit
+        if(matcher2.find()==true){
+            rules++;
+        }
+        String reg3 ="[a-zA-Z]";
+        Pattern pattern3 = Pattern.compile(reg2);
+        Matcher matcher3 = pattern3.matcher(pw);
+        //both upper and lower case
+        if(matcher3.find()==true){
             rules++;
         }
         return rules;
--- app/src/test/java/com/example/acme/csci3130_demo/ValidatorUnitTest.java
+++ app/src/test/java/com/example/acme/csci3130_demo/ValidatorUnitTest.java
@@ -8,9 +8,9 @@ public class ValidatorUnitTest {
 
     @Test
     public void validation() {
-            assertEquals(Validator.validation("password"),1);
-            assertEquals(Validator.validation("1234567"),1);
-            assertEquals(Validator.validation("HelloWorld"),2);
-
+        assertEquals(Validator.validation("password"),1);
+        assertEquals(Validator.validation("1234567"),3);
+        assertEquals(Validator.validation("HelloWorld"),2);
+        assertEquals(Validator.validation("H@allll11"),4);
     }
 }
